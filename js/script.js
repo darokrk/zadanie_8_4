09757e69bbe3a409e7fc4dd3f1500d4519f05c78
Dariusz Rosiek <daro.krk@interia.pl>
2017-12-25 23:49:30 +0100

    Init project files, putt example commits in js/script.js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// scripts.js
+
+var name = prompt('Enter your name');
+// example commit alert('Hello, ' + name);
+console.log('Hello, ' + name);
+/*
+  example commit 2
+*/
+//example commit 3
+//example commit 4
+
